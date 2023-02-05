9f4832ed1f7bf2d07d6534d9d9c9bfae2dadc3e4
Joe Lin <joelin3478@gmail.com>
2023-02-05 13:38:50 -0500

    Refactor styles, add .prettierrc and theme.tsx

---
+++ src/styles/theme.tsx
@@ -0,0 +1,31 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const fonts = { sans: `'-apple-system', sans-serif`,
+                mono: `'Menlo', monospace` }
+
+const breakpoints = {
+  sm: '40em',
+  md: '52em',
+  lg: '64em',
+  xl: '80em',
+}
+
+
+const theme = extendTheme({
+  semanticTokens: {
+    colors: {
+      text: {
+        default: '#16161D',
+        _dark: '#ade3b8',
+      },
+    },
+  },
+  colors: {
+    black: '#16161D',
+    royalblue: '#0070f3',
+  },
+  fonts,
+  breakpoints,
+})
+
+export default theme
