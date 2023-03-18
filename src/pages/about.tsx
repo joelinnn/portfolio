e543b78ba4655ca3ae560578a528df02224b8395
Joe Lin <joelin3478@gmail.com>
2023-03-18 18:49:52 -0400

    add basic components to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,12 @@
+import { Container } from "@chakra-ui/react"
+import { Header, Description } from "@/components"
+
+const AboutDescription = 'About me Page'
 export default function About () {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <Container maxW='850px' paddingTop='3em'>
+      <Header />
+      <Description title='About Me' description={AboutDescription}/>
+    </Container>
   )
 }
